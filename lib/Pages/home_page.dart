8a688d78c2f39c00521c7e71c0edbfac2a408b4c
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-10-24 15:37:36 -0700

    initialize smile id

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:smile_id/smile_id.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+    // replace with your own credentials
+    SmileID.initialize(
+      useSandbox: false,
+    );
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Container(
+        child: Center(
+          
+        ),
+      )),
+    );
   }
 }
